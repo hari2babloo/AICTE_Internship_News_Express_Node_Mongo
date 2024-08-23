0ee48bdabc5a790f8ebb2288b157c745bfc88c4d
Hari2babloo <hari2babloo@live.com>
2024-08-23 12:59:20 +0530

    week4

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+
 const News = () => {
   const [news, setnews] = useState([]);
   const navigate = useNavigate();
